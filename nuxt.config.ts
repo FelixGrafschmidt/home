4de22769860ac80b7c03b6d93925ad9205d684fd
FelixGrafschmidt <drache.dragon@gmail.com>
2022-11-14 18:45:54 +0100

    try proper deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { presetScrollbar } from "unocss-preset-scrollbar";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	ssr: false,
 	app: {
 		head: {
 			title: "Felix Grafschmidt - Homepage",
