46e5e85e2cc83b43282cd4bcac018a8187f722c7
FelixGrafschmidt <drache.dragon@gmail.com>
2022-11-22 20:08:57 +0100

    basic structure for character info

---
+++ components/global/characters/MasanaIG.vue
@@ -0,0 +1,215 @@
+<template>
+	<div>
+		<div font-italic mt-8 mx-16 text-center flex="~ col" gap-2>
+			<span font-semibold>
+				Niemand wird perfekt geboren. Jeder kann nur nach Perfektion streben, ist er zu Beginn auch noch so weit von ihr entfernt.
+			</span>
+			<span> -- Nakua Tohos </span>
+		</div>
+		<div flex="~ row" justify-between my-8 mx-16 gap-4>
+			<div w="1/2" flex="~ col" gap-4 leading-8>
+				<span> Aufgewachsen unter Menschen, nie wirklich verstanden, immer der Fremde, der Andere, der Seltsame… </span>
+				<span>
+					So sehr seine Mutter seinen Vater auch liebte, so sehr war Masana selbst ihr egal. Die Zeiten, die er in Riva verbringen
+					musste, waren ihm verhasst. Niemand unter den Menschen machte auch nur den Versuch, ihn zu verstehen. Dabei war er doch
+					gar nicht anders als sie. In der tiefen Überzeugung, dass er nur in irgendetwas gut genug sein müsste, damit andere ihn
+					mögen, begann er sein ganzes Streben auf die Verbesserung seiner selbst auszurichten. Zunächst versuchte er noch, die in
+					ihm wirkenden astralen Mächte zu kontrollieren, musste dabei aber feststellen, dass es ihm nicht gelang, diese seinem
+					Willen zu unterwerfen. Eine unberechenbare Kraft war das Letzte, was er gebrauchen konnte, betrachteten ihn doch schon
+					so alle in seiner Umgebung mit tiefem Misstrauen. Also beschloss er stattdessen, seinen Körper zu stählen. Von Natur aus
+					eher von schlankem Wuchs, war für ihn selbst wie auch für die in seiner Umgebung die Kraft, die in ihm heranwuchs, eine
+					Überraschung. Es gelang ihm, einigen Menschen in seiner Umgebung eine Art widerwilligen Respekt abzutrotzen. Aber mit
+					der Zeit wurde ihm klar, dass dieser mit seinem zunehmenden Alter immer stärker in Richtung Angst umschlug. Man
+					fürchtete den jungen Mann, der keine Freunde hatte und immerzu Streit suchte. Zutiefst desillusioniert von seinen
+					Mitmenschen sah er keinen anderen Ausweg als seine Heimat zu verlassen und zu versuchen, an einem anderen Ort, wo ihm
+					vielleicht mehr Verständnis entgegengebracht werden würde, zu versuchen, Frieden zu finden. Mit nichts als einem
+					einfachen Schwert und der Kleidung, die er an seinem Körper trug, machte er sich nach Süden auf. Ihm war das ewige Grau
+					des Nordens verhasst geworden.
+				</span>
+				<span>
+					Hin und wieder bot man ihm Arbeit an, meistens als Leibwache. Aber bald schon wurde ihm klar, dass das Kämpfen ihn nicht
+					glücklich machte.
+				</span>
+				<span> So zog ein Jahr ins Land… </span>
+				<span>
+					Nach einem weiteren langen Tag der Selbstertüchtigung, einem weiteren langen Tag der Stählung, hörte er hinter sich eine
+					Stimme. Für einen Moment war er verärgert, er hatte nur noch ein paar Meilen zurücklegen wollen, bevor er sich zur Ruhe
+					niederlegte, und er hatte überhaupt keine Lust, von einem Fremden angesprochen zu werden, der ihn letzten Endes auch
+					wieder nicht verstehen würde. Kurz dachte er darüber nach dem Mann den Kopf zurechtzustutzen, beschloss dann aber, dass
+					ihm das letzten Endes auch nicht zum Vorteil gereichen würde. Als er sich umwandte, um dem Mann zu antworten, war sein
+					erster Eindruck der von stark konzentrierter Kraft, wobei er nicht benennen konnte, ob es sich dabei um mentale oder
+					physische Kraft handelte. Neugierig geworden, bot er ihm an, sein Abendmahl zu teilen, was dieser dankend annahm.
+				</span>
+			</div>
+			<div w="1/2" flex="~ col" gap-4 leading-8>
+				<span>
+					Aufgewachsen unter Elfen, nie wirklich getraut, immer der Schwache, der Unbeholfene, der Unstete, der Ungewusste, der
+					Unbedachte…
+				</span>
+				<span>
+					Die eisigen Weiten des aventurischen Nordens lassen viel Raum, um sich seiner Selbst und seiner Umgebung bewusst zu
+					werden. Und dies war der Pfad, den Masanas Vater für ihn erdacht hatte. Immer wieder begleitete ihn sein Vater hinaus,
+					um ihn vorzubereiten auf die Gefahren des Eises und die Gefahren dessen, was jenseits des Eises lag. Und je mehr Zeit
+					Masana dort verbrachte, desto mehr wurde die Härte und die Kälte ein Teil von ihm. Hätte sein Vater doch nur einmal
+					bemerkt, dass es nicht die Kälte war, nach der Masana sich sehnte, sondern die Wärme und Nähe eines ihn liebenden
+					Wesens, vielleicht wäre auch sein Platz in der Sippe ein anderer gewesen. So aber gelang es ihm nie, auch nur zu einem
+					von ihnen eine Bindung aufzubauen. Im besten Fall betrachteten sie ihn mit Mitleid, meistens aber mit Argwohn, Furcht
+					oder sogar Abscheu. Mit der Zeit wurde Masana klar, dass dies kein Ort war, an dem er bleiben konnte. Von Schnee und Eis
+					hatte er für den Moment genug und so zog er gen Süden.
+				</span>
+				<span>
+					Doch nirgendwo war es ihm vergönnt, auch nur ein Gefühl von Heimat, Wärme oder Nähe zu finden. Oft bewegte er sich
+					abseits der Strassen, um so wenig Reisenden wie möglich begegnen zu müssen. Zwar würde ihm die Natur auch niemals
+					Freundlichkeit entgegenbringen können, aber er konnte zumindest versuchen, von ihr zu lernen.
+				</span>
+				<span> So zog ein Jahr ins Land… </span>
+				<span>
+					Nach einem weiteren langen Tag der Wanderung, einem weiteren langen Tag allein mit seinen Gedanken, seinen Ängsten,
+					seinen Sorgen, hörte er hinter sich eine Stimme. Er konnte zwar die Worte verstehen, musste aber davon ausgehen, dass er
+					nicht gemeint sein konnte. Niemand hier hätte einen Grund ihn anzusprechen. Aber nachdem er diesen Ruf wieder und wieder
+					hörte, wandte er sich schliesslich um und sah vor sich einen älteren Mann, ausser Atem, aber mit durchdringenden Blick.
+					Im ersten Moment verunsichert, besann er sich aber doch schnell darauf, dass dieser Mann etwas von ihm wollte. So bot er
+					ihm an, sein Abendmahl zu teilen, was dieser dankend annahm.
+				</span>
+			</div>
+		</div>
+		<div font-italic mx-16 text-center flex="~ col" gap-2>
+			<span font-semibold>
+				Die Natur ist der beste Lehrmeister. Nichts und niemand sonst besitzt die gleiche Vielzahl an Facetten und schafft es diese
+				auch in solcher Tiefe auszufüllen.
+			</span>
+			<span> -- Nakua Tohos </span>
+		</div>
+		<div flex="~ row" justify-between my-8 mx-16 gap-4>
+			<div w="1/2" flex="~ col" gap-4 leading-8>
+				<span>
+					«Du weisst nicht, wer du bist. Suche in dir, denn nur wer sich selbst kennt kann auch hoffen die Welt zu kennen».
+					Welches Interesse hatte er daran die Welt zu kennen? Hatte sie nicht deutlich gezeigt, dass sie kein Interesse an ihm
+					hatte? Wieso sollte er jetzt Zeit darauf verschwenden sein Inneres zu verstehen, wenn ihm das doch letzten Endes nichts
+					nützen würde? Nein, er täte besser daran seine Fähigkeiten im Schwertkampf zu verbessern. Nur das würde es ihm erlauben
+					in dieser Welt zu überleben. Eigentlich hatte er die Welt schon verstanden, egal, was der alte Mann gesagt hatte. Aber
+					schien ein fähiger Kämpfer zu sein und hatte zugestimmt ihn ein Jahr lang zu begleiten und ihm vielleicht den ein oder
+					anderen Trick zu zeigen.
+				</span>
+				<span> So zogen weitere acht Monate ins Land… </span>
+				<span>
+					«Wenn ich nicht bin wie meine Mutter und auch nicht bin wie mein Vater, dann muss ich etwas anderes sein, oder nicht?».
+					Der Gedanke war Masana eines Tages gekommen und er nahm an, dass er damit das Rätsel, dass der Fremde ihm gestellt
+					hatte, beantwortet hätte. Aber dieser lachte nur und sagte ihm, er habe gerade einmal die Frage gefunden. Sein
+					eigentliches Ziel sei die Antwort auf diese Frage. So sehr es Masana auch missfiel, die Vorstellung dass es eine Frage
+					gab, die ihm einen wesentlichen Teil seiner Selbst offenbaren könnte, erfüllte ihn mit Neugier.
+				</span>
+			</div>
+			<div w="1/2" flex="~ col" gap-4 leading-8>
+				<span>
+					«Du weisst nicht, wer du bist. Suche in dir, denn nur wer sich selbst kennt kann auch hoffen die Welt zu kennen». Seine
+					Worte klangen lange in Masana nach. Was sollte es bedeuten «sich selbst zu kennen»? Wen ausser sich selbst sollte man
+					überhaupt kennen können? Der Mann hatte versprochen ihm das nächste Jahr über bei dieser Suche zu helfen und wenn es ihm
+					gelänge, würde er ihn danach als Schüler annehmen. Ob dies das war, was er wollte, wusste Masana nicht.
+				</span>
+				<span> So zogen weitere acht Monate ins Land… </span>
+				<span>
+					«Wenn ich nicht bin wie mein Vater und auch nicht bin wie meine Mutter, dann muss ich etwas anderes sein, oder nicht?».
+					Immer wieder kehrten seine Gedanken zu diesem Punkt zurück. Es fühlte sich an, als läge in dieser Frage der Schlüssel zu
+					seinem Selbst. Aber niemals konnte er sie beantworten. Und auch sein Begleiter weigerte sich ihm zu helfen, obwohl er
+					ihm deutlich gemacht hatte, dass die Antwort nicht kompliziert sei. Was blieb ihm anderes übrig als weiter daran zu
+					knobeln?
+				</span>
+			</div>
+		</div>
+		<div flex="~ col" my-8 mx-16 gap-4 leading-8>
+			<span>
+				«Es ging niemals darum, ob ich das eine oder das andere bin, ich bin BEIDES! Und Nakua sagt, dass es auch richtig so ist,
+				weil alles in unserer Welt aus zwei Aspekten besteht. Mir ist noch nicht ganz klar, was er damit meint, aber ich glaube zu
+				verstehen, was es für mich bedeutet. Ich habe immer nur versucht so zu sein wir mein Vater oder meine Mutter. Es kam mir nie
+				in den Sinn, das mein Selbst aus beidem besteht. Rückblickend ist mir klar, dass meine Umgebung für diese Erkenntnis nicht
+				gerade hilfreich war, in Riva hat man in mir immer nur den Fremden, den Elfen gesehen, und bei der Sippe meines Vaters war
+				es genau umgekehrt. Ich fühle mich, als wäre die gesamte Last meiner 20 Jahre von mir genommen. Nur was soll ich mit dieser
+				neu erworbenen Erkenntnis, dieser Freiheit, diesem neuen Leben tun? Vielleicht hat Nakua eine Idee.»
+				<br />
+				Zum ersten Mal in seinem Leben hatte Masana das Gefühl sich selbst zu verstehen, mit sich selbst im Reinen zu sein, ein
+				Gefühl von Gleichgewicht, das anders war als alles, was er bisher erlebt hatte. Als er Nakua davon erzählte, lächelte dieser
+				nur geduldig und sagte ihm, dass er damit den ersten Schritt auf dem langen Weg zurückgelegt habe ein «Gutes Leben» zu
+				führen.
+			</span>
+			<span> Vier weitere Jahre vergingen… </span>
+			<span>
+				Wie sehr sich sein Leben doch verändert hatte. Zwar verbrachte er immer noch jeden Tag mehrere Stunden damit seine
+				Schwerttechnik zu verfeinern, aber alles, von seiner Grundtechnik, zu seiner Motivation und auch seiner Waffenwahl hatte
+				sich geändert. Er war nicht länger der ungestüme Jüngling, der mit einem billigen Eisenschwert in der Hand wild auf alle
+				einprügelte, was ihm irgendwie missfiel, nein. Wie in allem, was Nakua ihm beigebracht hatte, war auch im Kampf die erste
+				Lektion, dass alles im Universum aus zwei Aspekten besteht. Aus diesem Grund hatte er Masana zunächst mit blossen Händen
+				kämpfen lassen. «Denn bei wenig anderem ist uns ein so natürliches Gespür für die Dualität der Dinge gegeben». Dies sorgte
+				dafür, dass Masana schon sehr bald deutlich vorsichtiger wurde, wenn es darum ging sich seinem Gegenüber zu nähern. Erst als
+				er diese Lektion zu Nakuas Zufriedenheit verinnerlicht hatte, erlaubte dieser ihm wieder eine Waffe zu führen. Aber auch
+				hier war von Anfang an klar, dass er nicht wie in der Vergangenheit ein Schwert führen würde, sondern zwei. Auch wenn die
+				Übungen im waffenlosen zweihändigen Kampf ihn zu einem gewissen Grade darauf vorbereitet hatten, fehlte ihm doch die gesamte
+				Balance, von der er gedacht hatte, dass er sie sich erworben hatte.
+				<br />
+				Und so füllte der Dualismus langsam auch seinen Kampfstil. Nakua, sehr zufrieden mit dem Fortschritt seines Schülers,
+				beschloss, dass es Zeit war Masana den nächsten Schritt unternehmen zu lassen. Und so sassen sie eines Abends zusammen am
+				Feuer und Nakua begann von Rur und Gror zu sprechen. Für Masana, der in den letzten Jahren dieser Gedankenwelt immer näher
+				gekommen war, stellten diese Informationen eine willkommene, wenn auch nicht unerwartete, Ergänzung seines Weltbildes dar.
+				Sie erlaubten ihm die Welt aus einem grösseren Blickwinkel zu betrachten.
+				<br />
+				Die wichtigste Lektion, die Nakua ihm erteilt hatte, betraf Perfektion. Aufgrund der hohen Anforderungen an Disziplin,
+				Konzentration und Genauigkeit hatte Masana immer wieder das Gefühl, dass Nakua versuchte ihn in diese Richtung zu
+				beeinflussen. Als er ihn darauf ansprach, lächelte dieser nur sein geduldiges Lächeln und sagte: «Du hast die letzten vier
+				Jahre mit intensivem Studium hier in der Natur verbracht. Du hast die Falken beobachtet, den Bergbächen gelauscht und mit
+				den Felsen gewartet. Du hast gelernt alle Facetten der Natur zu respektieren und du wirst auch von ihr respektiert. Du hast
+				auf deine Art und Weise den hier zugrunde liegenden Dualismus begriffen. Deine Abstammung und Erziehung, so schmerzhaft die
+				Erinnerungen daran für sich auch sein mögen, waren dir dabei behilflich. Aber wie du möglicherweise schon vermutet hast,
+				kann die Natur alleine nicht zu Perfektion führen, nein. Es wird der Tag kommen, und er ist nicht mehr fern, an dem du den
+				Schritt in die zweite Hälfte der Welt tun musst. Und hier wird dir deine Abstammung nicht helfen können. Es werden
+				Erfahrungen sein, die dir vollkommen fremd sind, aber mit den dir gegebenen Fähigkeiten, zwar auch den angeborenen, aber
+				insbesondere den der letzten vier Jahre, bin ich überzeugt, dass du dich auch in dieser Welt zurechtfinden und, mit der
+				Zeit, auch meistern wirst».
+				<br />
+				Diese Offenbarung erstaunte Masana. Zwar hatte er begriffen, dass alles in der Welt einem Dualismus folgt, aber er hatte nie
+				darüber nachgedacht, dass auch das Leben selbst sich als solcher begreifen liess. Je mehr er darüber nachdachte, desto mehr
+				spürte er eine Unsicherheit in sich, ja sogar eine Angst. Sein letzter längerer Aufenthalt ausserhalb der Natur war in
+				seiner Jugend gewesen und er hatte keine positiven Erinnerungen an diese Zeit. Mit dem Bewusstsein, dass sein friedliches
+				Leben möglicherweise bald vorbei sein könnte, konzentrierte Masana sich um so mehr auf seine Lektionen, um die ihm
+				verbliebene Zeit optimal zu nutzen.
+			</span>
+			<span> Aber schon bald kam der Tag... </span>
+		</div>
+		<div font-italic mx-16 text-center flex="~ col" gap-2>
+			<span font-semibold> Der perfekte Krieger muss auch der perfekte Herrscher sein. </span>
+			<span> -- Nakua Tohos </span>
+		</div>
+		<div flex="~ col" my-8 mx-16 gap-4 leading-8>
+			<span>
+				«Was ist der Sinn deines Kämpfens?» Die Frage traf Masana unvorbereitet. Er hatte angenommen, dass das Kämpfen in sich
+				keinen Sinn habe, sondern dass es nur ein Mittel wäre. Als er diese Ansicht äusserte, zeigte Nakua sich zuerst enttäuscht.
+				Er hatte gehofft in den letzten vier Jahren einen Funken in Masana entzündet zu haben für die höheren Ideale der Kampfkunst.
+				Aber dann wurde ihm bewusst, dass er ihm genau das verwehrt hatte, als er ihn von kultivierter Gesellschaft ferngehalten
+				hatte. Also erklärte er ihm, dass es zwei Wege gäbe, den tieferen Sinn des Kämpfens zu begreifen und das vollständige
+				Meisterschaft nur durch das Beschreiten beider Wege möglich sei. Der erste Weg lag in der Betrachtung einzelner Handlungen:
+				Die Vollkommenheit zweier Schwertstreiche, das Zusammenspiel von Körper und Geist in jeder Bewegung. Der zweite Weg lag in
+				der Intention des Kämpfenden. Rur hatte den Weltendiskus zwar vollkommen erschaffen, aber leider gab es dennoch Mächte, die
+				danach strebten diese Schöpfung zu stören. Masana dachte lange über diese Worte nach und sie führten ihn schliesslich zu der
+				Frage, ob er sich nicht bereits auf diesen Wegen befände. Nakua aber offenbarte ihm, dass er glaube, dass Masana über sein
+				jetziges Wesen hinauswachsen könnte. Beinahe jeder könnte gewissen Fortschritt auf einem der Wege machen, aber Masana hatte
+				die richtigen Anlagen beide zu Ende zu gehen. Aber zu diesem Zwecke müsste er auch den Umgang mit Anderen lernen, auf
+				längere Sicht nicht nur als Untergebener oder Gleichgestellter sondern auch als Anführer.
+				<br />
+				Und so begann der zweite Teil Masanas Ausbildung. Zusammen mit Nakua bereiste er die grösseren Städte Aventuriens, sprach
+				mit vielen der Personen, die das Leben in Aventurien nachhaltig formten und beeinflussten und mass sich wieder und wieder
+				mit anderen in der Kunst des Schwertkampfs. Zunächst war es für Masana sehr verwunderlich, dass Nakua, den er als
+				Wanderprediger der Kirche der Zwillinge kennengelernt hatte, so viele Kontakte in der Zivilisation hatte. Aber schon bald
+				war der Gedanke daran vergessen, zu viele neue Eindrücke begegneten Masana jeden Tag. Nakua hatte ihm aufgetragen zu lesen.
+				Er sagte, dass ein Anführer gebildet sein müsste, damit alle seine Gefolgsleute ihn respektieren. Also las Masana. Werke
+				über Naturphilosophie, theologische Werke, natürlich auch Schriften über Strategie und Taktik. Und langsam, im Laufe der
+				Monate, begann Masana sich in diese Welt einzuleben. Er begann Gesellschaft zu geniessen und genoss es geistreiche Gespräche
+				mit anderen zu führen. Sein Verhältnis zu Nakua war von den klaren Regeln einer Lehrer-SchülerBeziehung geprägt gewesen, was
+				es ihm nur sehr selten erlaubt hatte unbeschwert über Dinge zu reden oder auch nur mehr Zeit in seinen Gedanken mit ihnen zu
+				verbringen.
+				<br />
+				Und so fand Masana, nach Jahren, die er fernab der Zivilisation geführt hatte, den Weg zu ihr zurück. Zweifelsohne war er in
+				fast jeder Hinsicht eine andere Person als zuvor. Aber genau so sicher war es, dass er vorbereitet war, auf das, was noch
+				kommen würde.
+			</span>
+		</div>
+	</div>
+</template>
--- pages/pnp.vue
+++ pages/pnp.vue
@@ -1,31 +1,204 @@
 <template>
 	<main flex="col ~" gap-4>
 		<span text-2xl font-semibold>WIP - My Characters</span>
+		<span text-lg> I have been playing Pen-and-Paper-RPGs since 2010 and I am loving them! </span>
 		<section mb-4 flex="~ col" gap-2>
-			<nuxt-link
-				v-for="(character, i) in characters"
-				:key="i"
-				target="_blank"
-				text-xl
-				cursor-pointer
-				hover:text-teal-4
-				:to="character.url"
-			>
-				{{ character.name }}
-			</nuxt-link>
+			<div v-for="(character, i) in characters" :key="i" target="_blank" rounded border>
+				<div p-2 cursor-pointer text-xl @click="selectCharacter(character)">
+					{{ character.name }}
+				</div>
+				<div v-if="character.active" rounded-b bg-gray-8>
+					<div flex="~ col" px-2>
+						<span>Species: {{ character.species }}</span>
+						<span>Profession: {{ character.profession }}</span>
+					</div>
+					<div flex="~ row" gap-2 items-center px-2>
+						<span>Character Sheet:</span>
+						<nuxt-link :to="character.sheets.get(System.TDE)" hover:text-teal-4 target="_blank" underline text-sm
+							>TDE</nuxt-link
+						>
+						<nuxt-link :to="character.sheets.get(System.DND)" hover:text-teal-4 target="_blank" underline text-sm
+							>DnD</nuxt-link
+						>
+					</div>
+					<div flex="~ row" justify-around border-t mt-2 items-center>
+						<span
+							:class="character.tab === Tab.IG ? 'bg-gray-7 border-b-gray-7' : 'border-b-gray-3'"
+							border-b
+							cursor-pointer
+							py-2
+							text-center
+							border-r
+							w="1/2"
+							@click="character.tab = Tab.IG"
+						>
+							In Game
+						</span>
+						<span
+							:class="character.tab === Tab.OOG ? 'bg-gray-7 border-b-gray-7' : 'border-b-gray-3'"
+							border-b
+							cursor-pointer
+							py-2
+							text-center
+							w="1/2"
+							@click="character.tab = Tab.OOG"
+						>
+							Out of Game
+						</span>
+					</div>
+					<div bg-gray-7 p-2 rounded-b>
+						<component :is="character.description.get(character.tab)" />
+					</div>
+				</div>
+			</div>
 		</section>
 	</main>
 </template>
 
 <script lang="ts" setup>
-	const characters = [
-		{ name: "Masana Ruhige-Hand-Ruhiger-Geist", url: "https://www.dndbeyond.com/characters/83758189" },
-		{ name: "Laryllan Abendbringer", url: "https://www.dndbeyond.com/characters/87364170" },
-		{ name: "Pergimtosch groscho Pergschosch", url: "https://www.dndbeyond.com/characters/88340988" },
-		{ name: "Yuan Flaminjo Kjaskar Raskirson", url: "https://www.dndbeyond.com/characters/88341155" },
-		{ name: "Cordowan von Löwenhaupt", url: "https://www.dndbeyond.com/characters/88341380" },
-		{ name: "Ithambar Glanz-der-Nacht", url: "https://www.dndbeyond.com/characters/88341499" },
-		{ name: "Taren groscho Bronn", url: "https://www.dndbeyond.com/characters/88341606" },
-		{ name: "Hesindian Sindaru", url: "https://www.dndbeyond.com/characters/88341786" },
-	];
+	import { Ref } from "vue";
+
+	enum Tab {
+		IG = "IG",
+		OOG = "OOG",
+	}
+	enum System {
+		DND = "DnD",
+		TDE = "TDE",
+	}
+	interface Character {
+		name: string;
+		sheets: Map<System, string>;
+		species: string;
+		profession: string;
+		active: boolean;
+		tab: Tab;
+		description: Map<Tab, string>;
+	}
+	const characters: Ref<Character[]> = ref([
+		{
+			tab: Tab.IG,
+			species: "Half-Elf",
+			profession: "Mercenary",
+			active: false,
+			description: new Map<Tab, string>([
+				[Tab.IG, "CharactersMasanaIG"],
+				[Tab.OOG, "CharactersMasanaOOG"],
+			]),
+			sheets: new Map<System, string>([
+				[System.DND, "https://www.dndbeyond.com/characters/83758189"],
+				[System.TDE, "/characters/Masana.html"],
+			]),
+			name: "Masana Ruhige-Hand-Ruhiger-Geist",
+		},
+		{
+			tab: Tab.IG,
+			species: "Half-Elf",
+			profession: "Bard",
+			active: false,
+			description: new Map<Tab, string>([
+				[Tab.IG, "CharactersLaryllanIG"],
+				[Tab.OOG, "CharactersLaryllanOOG"],
+			]),
+			name: "Laryllan Abendbringer",
+			sheets: new Map<System, string>([
+				[System.DND, "https://www.dndbeyond.com/characters/87364170"],
+				[System.TDE, "CharactersMasanaOOG"],
+			]),
+		},
+		{
+			tab: Tab.IG,
+			species: "Anvil-Dwarf",
+			profession: "Mercenary",
+			active: false,
+			description: new Map<Tab, string>([
+				[Tab.IG, "CharactersPergimtoschIG"],
+				[Tab.OOG, "CharactersPergimtoschOOG"],
+			]),
+			sheets: new Map<System, string>([
+				[System.DND, "https://www.dndbeyond.com/characters/88340988"],
+				[System.TDE, "CharactersMasanaOOG"],
+			]),
+			name: "Pergimtosch groscho Pergschosch",
+		},
+		{
+			tab: Tab.IG,
+			species: "Human (Half Thorval, Half Brabakian)",
+			profession: "Black Mage",
+			active: false,
+			description: new Map<Tab, string>([
+				[Tab.IG, "CharactersYuanIG"],
+				[Tab.OOG, "CharactersYuanOOG"],
+			]),
+			name: "Yuan Flaminjo Kjaskar Raskirson",
+			sheets: new Map<System, string>([
+				[System.DND, "https://www.dndbeyond.com/characters/88341155"],
+				[System.TDE, "CharactersMasanaOOG"],
+			]),
+		},
+		{
+			tab: Tab.IG,
+			species: "Human",
+			profession: "Knight",
+			active: false,
+			description: new Map<Tab, string>([
+				[Tab.IG, "CharactersCordowanIG"],
+				[Tab.OOG, "CharactersCordowanOOG"],
+			]),
+			name: "Cordowan von Löwenhaupt",
+			sheets: new Map<System, string>([
+				[System.DND, "https://www.dndbeyond.com/characters/88341380"],
+				[System.TDE, "CharactersMasanaOOG"],
+			]),
+		},
+		{
+			tab: Tab.IG,
+			species: "Wood-Elf",
+			profession: "Ranger",
+			active: false,
+			description: new Map<Tab, string>([
+				[Tab.IG, "CharactersIthambarIG"],
+				[Tab.OOG, "CharactersIthambarOOG"],
+			]),
+			name: "Ithambar Glanz-der-Nacht",
+			sheets: new Map<System, string>([
+				[System.DND, "https://www.dndbeyond.com/characters/88341499"],
+				[System.TDE, "CharactersMasanaOOG"],
+			]),
+		},
+		{
+			tab: Tab.IG,
+			species: "Dwarf",
+			profession: "Air-Geode",
+			active: false,
+			description: new Map<Tab, string>([
+				[Tab.IG, "CharactersTarenIG"],
+				[Tab.OOG, "CharactersTarenOOG"],
+			]),
+			name: "Taren groscho Bronn",
+			sheets: new Map<System, string>([
+				[System.DND, "https://www.dndbeyond.com/characters/88341606"],
+				[System.TDE, "CharactersMasanaOOG"],
+			]),
+		},
+		{
+			tab: Tab.IG,
+			species: "Human",
+			profession: "Ilumnestrian Priest",
+			active: false,
+			description: new Map<Tab, string>([
+				[Tab.IG, "CharactersHesidianIG"],
+				[Tab.OOG, "CharactersHesidianOOG"],
+			]),
+			name: "Hesindian Sindaru",
+			sheets: new Map<System, string>([
+				[System.DND, "https://www.dndbeyond.com/characters/88341786"],
+				[System.TDE, "CharactersMasanaOOG"],
+			]),
+		},
+	]);
+
+	function selectCharacter(character: Character): void {
+		character.active = !character.active;
+	}
 </script>
